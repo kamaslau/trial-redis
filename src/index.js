ed1f64caa1bfa1e4bd96a3d9f313b22b75b5f3f7
Kamas Lau <kamaslau@outlook.com>
2022-06-01 07:31:24 +0800

    feat: 将服务启动为Koa

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import { createClient } from 'redis';
+import Koa from 'koa'
+import { createClient } from 'redis'
+const client = createClient()
+client.on('error', (err) => console.log('Redis Client Error', err))
 
-const client = createClient();
-client.on('error', (err) => console.log('Redis Client Error', err));
+const app = new Koa()
 
-await client.connect();
-await client.set('sample-key:lastRequestedAt', Date.now()/1000);
-const value = await client.get('sample-key:lastRequestedAt');
-console.log('result: ', value)
+app.use(async ctx => {
+  await client.connect()
+
+  const keyName = 'sample-key:lastRequestedAt'
+  const keyValue = Date.now()
+
+  await client.set(keyName, keyValue)
+  const value = await client.get(keyName)
+
+  client.disconnect()
+
+  ctx.body = { key: keyName, value }
+})
+
+app.listen(3000)
